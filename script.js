c973e7a873d9b284b085d710eced8fd5c5c1d7e5
ng0035 <nikitaguptarules@gmail.com>
2018-05-13 22:09:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    function setColor() {
+
+        alert("hello!");
+        var property = document.getElementById("btn-ac");
+
+            property.style.backgroundColor = "#000000";
+            property.style.color = "#FFFFFF";
+
+        }
